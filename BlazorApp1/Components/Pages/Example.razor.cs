d62d2a72a1a262ef8c1950742c36550a78a8c163
admin <https://github.com/RiiikkiTikkiTavi>
2025-03-11 19:58:06 +0300

    Добавьте файлы проекта.

---
+++ BlazorApp1/Components/Pages/Example.razor.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+namespace BlazorApp1.Components.Pages
+{
+	public class ExampleModel : ComponentBase
+	{
+		//CregitInfoWS.CreditOrgInfoSoapClient ws = new CregitInfoWS.CreditOrgInfoSoapClient(); //ссылка на сервис
+		
+		/*ws.UseDefaultCredentials = true;
+            ws.CookieContainer = new System.Net.CookieContainer();
+           // ws.Url = "http://localhost:8084/CreditOrgInfo.asmx?WSDL"; // for local test
+            ws.Url = "http://www.cbr.ru/CreditInfoWebServ/CreditOrgInfo.asmx?WSDL";
+		*/
+
+		private string time = "";
+		private string val = "";
+
+		private int input = 0;
+		private double result = 0;
+		private string operation = "";
+		private double firstNumber;
+		private string display = "";
+
+		public string Time { get => time; set => time = value; }
+        public string Val { get => val; set => val = value; }
+		public string Display { get => display; set => display = value; }
+		public int Input { get => input; set => input = value; }
+
+		public void ShowTime()
+		{
+			Time = DateTime.Now.ToString("D");
+
+		}
+
+		public void SetOperation(string op)
+		{
+
+				operation = op;
+				Display += operation; 	
+		}
+
+		public void SetInput(int num) {
+			Display += num.ToString();
+			Input = 0;
+
+		}
+		public void Calculate()
+		{
+			if (/*!string.IsNullOrEmpty(Input) && */!string.IsNullOrEmpty(operation))
+			{
+				double secondNumber = 0;// = double.Parse(Input);
+				switch (operation)
+				{
+					case "+": result = firstNumber + secondNumber; break;
+					case "-": result = firstNumber - secondNumber; break;
+					case "*": result = firstNumber * secondNumber; break;
+					case "/": result = secondNumber != 0 ? firstNumber / secondNumber : 0; break;
+
+				}
+				//Input = result.ToString();
+				operation = "";
+			}
+		}
+
+		public void Clear()
+		{
+			Input = 0;
+			result = 0;
+			operation = "";
+			Display = "";
+		}
+
+
+	}
+	public enum Status
+	{
+		disconnected,
+		connected,
+		connecting,
+		working
+	}
+}
---
+++ BlazorApp1/CreditOrgInfoClient.cs
@@ -0,0 +1,67 @@
+﻿using global::CregitInfoWS;
+using Google.Protobuf.WellKnownTypes;
+using System;
+using System.Data;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace BlazorApp1
+{
+	
+		public class CreditOrgInfoClient
+		{
+			private readonly CreditOrgInfoSoap _client;
+
+			public CreditOrgInfoClient()
+			{
+				var binding = new BasicHttpBinding();
+				var endpoint = new EndpointAddress("http://www.cbr.ru/CreditInfoWebServ/CreditOrgInfo.asmx");
+
+				var factory = new ChannelFactory<CreditOrgInfoSoap>(binding, endpoint);
+				_client = factory.CreateChannel();
+			}
+
+	
+
+        // получение наименования банка по рег. номеру
+        public async Task<string> GetBankNameByRegnum(int regnum)
+        {
+            double internal_code = await _client.RegNumToIntCodeAsync(regnum);
+            if (internal_code == -1) return "";
+            XmlNode xmlResponse = await _client.CreditInfoByIntCodeXMLAsync(internal_code);
+
+            if (xmlResponse == null) return "";
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlResponse.OuterXml);
+
+            XmlNode? orgNameNode = xmlDoc.SelectSingleNode("//OrgName");
+            return orgNameNode?.InnerText ?? "";
+        }
+
+		// получение данных по форме 123 по рег. номеру банка и дате
+		public async Task<DataSet> GetData123(int regnum, DateTime dt)
+		{
+			var response = await _client.Data123FormFullAsync(regnum, dt);
+
+			if (response == null || response.Nodes == null || response.Nodes.Count == 0)
+				return null; // Если данные пустые
+
+			// Оборачиваем полученные XML-элементы в общий корневой узел
+			XDocument xmlDoc = new XDocument(new XElement("Root", response.Nodes));
+
+			// Создаем DataSet и загружаем в него XML
+			DataSet dataSet = new DataSet();
+			using (var reader = xmlDoc.CreateReader())
+			{
+				dataSet.ReadXml(reader);
+			}
+
+			return dataSet;
+		}
+	}
+	}
+
+
