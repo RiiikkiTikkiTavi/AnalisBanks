19497432901c7b4882e4fa5dd42e990d1a11d6e9
admin <https://github.com/RiiikkiTikkiTavi>
2025-03-19 21:54:59 +0300

    Реализован вспомогательный метод ShowDataSet для вывода dataset в консоль, Реализовано получение 101 формы без оборотов и ее вывод, исключая лишние столбцы

--- BlazorApp1/CreditOrgInfoClient.cs
+++ BlazorApp1/CreditOrgInfoClient.cs
@@ -25,6 +25,52 @@ namespace BlazorApp1
 		}
 
 
+		//вспомогательный метод - вывод dataset в консоль
+		private void ShowDataSet(DataSet dataSet)
+		{
+			/*foreach (DataTable table in dataSet.Tables)
+			{
+				Console.WriteLine($"Таблица: {table.TableName}");
+				foreach (DataColumn column in table.Columns)
+				{
+					Console.WriteLine(string.Join(" | ", column.ColumnName));
+				}
+
+				// Вывод заголовков колонок
+				Console.WriteLine(new string('-', 50)); // разделитель
+				foreach (DataRow row in table.Rows)
+				{
+					Console.WriteLine(string.Join(" | ", row.ItemArray)); // строки
+				}
+				foreach (DataRow row in dataSet.Tables[0].Rows.Cast<DataRow>())
+				{
+					foreach (var item in row.ItemArray)
+					{
+						Console.WriteLine(string.Join(" | ", item));
+					}
+				}
+
+			}*/
+			foreach (DataTable table in dataSet.Tables)
+			{
+				Console.WriteLine($"Таблица: {table.TableName}");
+
+				// Вывод заголовков колонок
+				Console.WriteLine(string.Join(" | ", table.Columns.Cast<DataColumn>().Select(col => col.ColumnName)));
+
+				// Разделитель
+				Console.WriteLine(new string('-', 50));
+
+				// Вывод всех строк
+				foreach (DataRow row in table.Rows)
+				{
+					Console.WriteLine(string.Join(" | ", row.ItemArray));
+				}
+
+				Console.WriteLine(new string('=', 50)); // Разделитель между таблицами
+			}
+		}
+
 
 		// получение наименования банка по рег. номеру
 		public async Task<string> GetBankNameByRegnum(int regnum)
@@ -64,6 +110,75 @@ namespace BlazorApp1
 			}
 			return dataSet;
 		}
+
+		// получение данных по форме 135 по рег. номеру банка и дате
+		public async Task<DataSet> GetData135(int regnum, DateTime dt)
+		{
+			var response = await _client.Data135FormFullAsync(regnum, dt);
+
+			if (response == null || response.Nodes == null || response.Nodes.Count == 0)
+				return null; // Если данные пустые
+
+			// Оборачиваем полученные XML-элементы в общий корневой узел
+			XDocument xmlDoc = new XDocument(
+				new XDeclaration("1.0", "utf-8", null), // Добавляем заголовок XML
+				new XElement("Root", response.Nodes)
+			);
+
+			DataSet dataSet = new DataSet();
+
+			Console.WriteLine(xmlDoc.ToString());
+			using (XmlReader xmlReader = xmlDoc.CreateReader())
+			{
+				dataSet.ReadXml(xmlReader); // Читаем XML
+			}
+			
+			return dataSet;
+		}
+
+		
+		// получение данных по форме 101 по рег. номеру банка и дате
+		public async Task<DataSet> GetData101(int regnum, DateTime dt)
+		{
+			// список колонок, которые необходимо оставить при выводе
+			List<string> columnsToKeep = new List<string> { "pln", "ap", "numsc", "vitg", "iitg", "priz", "Column1" };
+
+			var response = await _client.Data101FNewAsync(regnum, dt);
+
+			if (response == null || response.Nodes == null || response.Nodes.Count == 0)
+				return null; // Если данные пустые
+
+			// Оборачиваем полученные XML-элементы в общий корневой узел
+			XDocument xmlDoc = new XDocument(
+				new XDeclaration("1.0", "utf-8", null), // Добавляем заголовок XML
+				new XElement("Root", response.Nodes)
+			);
+
+			DataSet dataSet = new DataSet();
+
+			using (XmlReader xmlReader = xmlDoc.CreateReader())
+			{
+				dataSet.ReadXml(xmlReader); // Читаем XML
+			}
+			foreach (DataTable table in dataSet.Tables)
+			{
+				var columnsToRemove = table.Columns.Cast<DataColumn>() // преобразование table.Columns в IEnumerable<DataColumn>, чтобы применить LINQ.
+										.Select(c => c.ColumnName)  // получение списка имен столбцов
+										.Except(columnsToKeep)  // оставляет только те элементы, которые не входят в columnsToKeep
+										.ToList();
+
+				foreach (string columnName in columnsToRemove)
+				{
+					table.Columns.Remove(columnName); // удаление лишних колонок
+				}
+			}
+
+			ShowDataSet(dataSet);
+
+			return dataSet;
+		}
+		
+		
 	}
 }
 
